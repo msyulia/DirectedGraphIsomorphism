e6abe8ad8bef7df3fe9aefbfa03dce23babdc624
KowalikJakub <kowalik.kub@gmail.com>
2018-05-21 01:29:59 +0200

    Wstępny layout aplikacji
    -Dodano foldery na porządkowanie plików
    -Dodano kilka plików czekających na wypełnienie
    -Dodano w Pomoc->Źródło niespodziankę!
    -Zakapsułkowano operacje na grafach w klasie GraphOperation

---
+++ DGI/DGI/Controller/GraphOperation.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using DGI.Model;
+
+namespace DGI.Controller
+{
+    /// <summary>
+    /// Klasa służąca do przechowywania operacji na grafach.
+    /// Dla wszystkich operacji mierzony jest czas wykonywania 
+    /// </summary>
+    public class GraphOperation
+    {
+        /// <summary>
+        /// Delegat dla operacji na grafach.
+        /// Wszystkie operacje na grafach będą się wykonywać asynchronicznie.
+        /// </summary>
+        /// <param name="graph_1"></param>
+        /// <param name="graph_2"></param>
+        /// <returns></returns>
+        public delegate Task Operation(GraphModel graph_1, GraphModel graph_2);
+
+        private const string GRAPH_OPERATION_RESULT = "Obliczenia trwały: ";
+        private Operation graphOperation; 
+        private Stopwatch stopwatch;
+
+        public GraphOperation(Operation graphOperation)
+        {
+            this.graphOperation = graphOperation;
+        }
+
+        public async Task<string> Run(GraphModel graph_1, GraphModel graph_2)
+        {
+            string result = GRAPH_OPERATION_RESULT;
+            stopwatch.Start();
+
+            await graphOperation(graph_1, graph_2);
+
+            stopwatch.Stop();
+
+            result += stopwatch.ElapsedMilliseconds;
+            return result;
+        }
+    }
+}
---
+++ DGI/DGI/View/View.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace DGI.View
+{
+    public abstract class View
+    {
+        public abstract void DrawView(object sender, RoutedEventArgs e);
+    }
+}
